c01f6be1fb4c6ef3e83799718d4fac7bdac07a6c
Anastasiia <bardieranastasi@gmail.com>
2023-11-17 16:11:02 +0200

    Відредагувала відеоплеєр.Додала можливість завантажити файл

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener("DOMContentLoaded", () => {
   const slider = new MainSlider({ btns: ".next", container: ".page" });
@@ -41,10 +43,14 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   feedSlider.init();
 
-  const player = new VideoPlayer(".showup .play", ".overlay");
-  player.init();
+  new VideoPlayer(".showup .play", ".overlay").init();
+  new VideoPlayer(".module__video-item .play", ".overlay").init();
 
   new Difference(".officerold", ".officernew", ".officer__card-item").init();
 
   new Form(".form").init();
+
+  new ShowInfo(".plus__content").init();
+
+  new Download(".download").init();
 });
